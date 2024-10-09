96e4ba167bfa9bb28e4f0a5628e4fedd9629eff5
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-09 19:58:30 +0300

    true-false score has been dynamised

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,10 +10,13 @@ import {
   Row,
 } from "react-bootstrap";
 import wordList from "../../words.json";
+
 export class Home extends Component {
   constructor(props) {
     super(props);
+
     let newWordsList = wordList.sort(() => Math.random() - 0.5);
+
     this.state = {
       changeWord: "",
       checkWord: "",
@@ -25,16 +28,20 @@ export class Home extends Component {
       interval: null,
     };
   }
+
   componentDidMount() {
     let { words } = this.state;
-    this.setState({ checkWord: words[0] });
+
+    this.setState({
+      checkWord: words[0],
+    });
   }
 
   handleWord = (event) => {
     let { words, checkWord, trueCount, falseCount } = this.state;
     let getValue = event.target.value;
 
-    if (getValue !== "") {
+    if (getValue !== " ") {
       if (getValue.slice(-1) !== " ") {
         this.setState({
           changeWord: getValue,
@@ -42,25 +49,28 @@ export class Home extends Component {
       } else {
         this.setState(
           {
-            checkWord: getValue.slice(0, -1),
+            changeWord: getValue.slice(0, -1),
           },
           () => {
             if (checkWord === getValue.slice(0, -1)) {
+              words.shift();
+
               this.setState({
-                trueCount: trueCount + 1,
-                words: words,
-                checkWord: words[0],
                 changeWord: "",
+                checkWord: words[0],
+                words: words,
+                trueCount: trueCount + 1,
                 truePercent:
                   ((trueCount + 1) * 100) / (trueCount + 1 + falseCount),
               });
             } else {
               words.shift();
+
               this.setState({
-                falseCount: falseCount + 1,
-                words: words,
-                checkWord: words[0],
                 changeWord: "",
+                checkWord: words[0],
+                words: words,
+                falseCount: falseCount + 1,
                 truePercent: (trueCount * 100) / (trueCount + falseCount),
               });
             }
@@ -69,96 +79,107 @@ export class Home extends Component {
       }
     }
   };
+
   render() {
     let {
-      timer,
       words,
       changeWord,
       checkWord,
-      falseCount,
       trueCount,
+      falseCount,
       truePercent,
+      timer,
     } = this.state;
+
     return (
-      <Container className={"mt-5"}>
-        <Row>
-          <Col md={12}>
-            {timer >= 60 ? (
-              <Card>
-                <Card.Header>Fast Typing</Card.Header>
-                <Card.Body>
-                  <Card.Title>Hız Testi</Card.Title>
-                  <Card.Text>
-                    Zamandan kazanmak için parmaklarını test et :)
-                    <br />
-                    Doğru: {trueCount}
-                    <br />
-                    Yanlış: {falseCount}
-                  </Card.Text>
-                  <hr />
-                  <Card body className={"border-dark"}>
-                    {words.length > 0 &&
-                      words.slice(0, 50).map((item, index) => (
-                        <span
-                          style={{
-                            fontSize: "15px",
-                            fontWeight: 600,
-                            whiteSpace: "nowrap",
-                            display: "inline-block",
-                          }}
-                          className={`${
-                            index === 0
-                              ? checkWord.match(changeWord)
-                                ? "bg-secondary text-white"
-                                : "bg-danger text-white"
-                              : "text-dark"
-                          } p-1 mx-1 my-1 rounded`}
-                        >
-                          {item}
-                        </span>
-                      ))}
-                  </Card>
-                  <hr />
-                  <Card body>
-                    <InputGroup>
-                      <FormControl placeholder={"Kelimeyi Yazınız..."} />
-                      <InputGroup className={"mt-3"}>
-                        <Button disabled={true} variant={"outline-secondary"}>
-                          30 sn.
-                        </Button>
-                        <Button variant={"outline-success ml-2"}>Yenile</Button>
+      <>
+        <Container className={"mt-5"}>
+          <Row>
+            <Col md={12}>
+              {timer >= 60 ? (
+                <Card>
+                  <Card.Header>mType</Card.Header>
+                  <Card.Body>
+                    <Card.Title>mType Hız Testi</Card.Title>
+                    <Card.Text>
+                      Zamandan kazanmak için parmaklarını test et :)
+                      <br />
+                      Doğru: {trueCount}
+                      <br />
+                      Yanlış: {falseCount}
+                    </Card.Text>
+                    <hr />
+                    <Card body className={"border-dark"}>
+                      {words.length > 0 &&
+                        words.slice(0, 50).map((item, index) => (
+                          <span
+                            style={{
+                              fontSize: "15px",
+                              fontWeight: 600,
+                              whiteSpace: "nowrap",
+                              display: "inline-block",
+                            }}
+                            className={`${
+                              index === 0
+                                ? checkWord.match(changeWord)
+                                  ? "bg-secondary text-white"
+                                  : "bg-danger text-white"
+                                : "text-dark"
+                            } p-1 mx-1 my-1 rounded`}
+                          >
+                            {item}
+                          </span>
+                        ))}
+                    </Card>
+                    <hr />
+                    <Card body>
+                      <InputGroup>
+                        <FormControl
+                          value={changeWord}
+                          onChange={this.handleWord}
+                          placeholder={"Kelimeyi Yazınız..."}
+                        />
+                        <InputGroup className={"mt-3"}>
+                          <Button disabled={true} variant={"outline-secondary"}>
+                            {timer}
+                            sn.
+                          </Button>
+                          <Button variant={"outline-success ml-2"}>
+                            Yenile
+                          </Button>
+                        </InputGroup>
                       </InputGroup>
-                    </InputGroup>
-                  </Card>
-                  <hr />
-                </Card.Body>
-              </Card>
-            ) : (
-              <Card>
-                <Card.Header>Fast Typing</Card.Header>
-                <Card.Body>
-                  <Card.Title>Hız Testi Sonucu</Card.Title>
-                  <Card.Text>
-                    <Alert variant={"success"}>
-                      <h4>Oyun bitti :)</h4>
-                      <br />
-                      <font style={{ fontSize: "50px" }}>60 DKS</font>
-                      <br />
-                      Doğruluk Yüzdesi: 80%
-                      <br />
-                      Doğru Kelime: 50
-                      <br />
-                      Yanlış Kelime: 10
-                      <br />
-                      <Button variant={"primary mt-2"}>Yeni Oyun</Button>
-                    </Alert>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            )}
-          </Col>
-        </Row>
-      </Container>
+                    </Card>
+                    <hr />
+                  </Card.Body>
+                </Card>
+              ) : (
+                <Card>
+                  <Card.Header>mType</Card.Header>
+                  <Card.Body>
+                    <Card.Title>mType Hız Testi Sonuçları</Card.Title>
+                    <Card.Text>
+                      <Alert variant={"success"}>
+                        <h4>Oyun bitti :)</h4>
+                        <br />
+                        <font style={{ fontSize: "50px" }}>60 DKS</font>
+                        <br />
+                        Doğruluk Yüzdesi: 80%
+                        <br />
+                        Doğru Kelime: 50
+                        <br />
+                        Yanlış Kelime: 10
+                        <br />
+                        <Button variant={"primary mt-2"}>Yeni Oyun</Button>
+                      </Alert>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
