515eac03d1292184007635ca3dc70faf690cdd64
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-09 19:53:34 +0300

    words listed on the screen

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,17 +9,76 @@ import {
   InputGroup,
   Row,
 } from "react-bootstrap";
+import wordList from "../../words.json";
 export class Home extends Component {
   constructor(props) {
     super(props);
-
+    let newWordsList = wordList.sort(() => Math.random() - 0.5);
     this.state = {
+      changeWord: "",
+      checkWord: "",
+      words: newWordsList,
+      trueCount: 0,
+      falseCount: 0,
+      truePercent: 0,
       timer: 60,
+      interval: null,
     };
   }
+  componentDidMount() {
+    let { words } = this.state;
+    this.setState({ checkWord: words[0] });
+  }
 
+  handleWord = (event) => {
+    let { words, checkWord, trueCount, falseCount } = this.state;
+    let getValue = event.target.value;
+
+    if (getValue !== "") {
+      if (getValue.slice(-1) !== " ") {
+        this.setState({
+          changeWord: getValue,
+        });
+      } else {
+        this.setState(
+          {
+            checkWord: getValue.slice(0, -1),
+          },
+          () => {
+            if (checkWord === getValue.slice(0, -1)) {
+              this.setState({
+                trueCount: trueCount + 1,
+                words: words,
+                checkWord: words[0],
+                changeWord: "",
+                truePercent:
+                  ((trueCount + 1) * 100) / (trueCount + 1 + falseCount),
+              });
+            } else {
+              words.shift();
+              this.setState({
+                falseCount: falseCount + 1,
+                words: words,
+                checkWord: words[0],
+                changeWord: "",
+                truePercent: (trueCount * 100) / (trueCount + falseCount),
+              });
+            }
+          }
+        );
+      }
+    }
+  };
   render() {
-    const { timer } = this.state;
+    let {
+      timer,
+      words,
+      changeWord,
+      checkWord,
+      falseCount,
+      trueCount,
+      truePercent,
+    } = this.state;
     return (
       <Container className={"mt-5"}>
         <Row>
@@ -32,25 +91,32 @@ export class Home extends Component {
                   <Card.Text>
                     Zamandan kazanmak için parmaklarını test et :)
                     <br />
-                    Doğru: 0
+                    Doğru: {trueCount}
                     <br />
-                    Yanlış: 0
+                    Yanlış: {falseCount}
                   </Card.Text>
                   <hr />
                   <Card body className={"border-dark"}>
-                    <span
-                      style={{
-                        fontSize: "15px",
-                        fontWeight: 600,
-                        whiteSpace: "nowrap",
-                        display: "inline-block",
-                      }}
-                      className={
-                        "bg-secondary text-white p-1 mx-1 my-1 rounded"
-                      }
-                    >
-                      asdsa
-                    </span>
+                    {words.length > 0 &&
+                      words.slice(0, 50).map((item, index) => (
+                        <span
+                          style={{
+                            fontSize: "15px",
+                            fontWeight: 600,
+                            whiteSpace: "nowrap",
+                            display: "inline-block",
+                          }}
+                          className={`${
+                            index === 0
+                              ? checkWord.match(changeWord)
+                                ? "bg-secondary text-white"
+                                : "bg-danger text-white"
+                              : "text-dark"
+                          } p-1 mx-1 my-1 rounded`}
+                        >
+                          {item}
+                        </span>
+                      ))}
                   </Card>
                   <hr />
                   <Card body>
